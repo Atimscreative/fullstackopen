f4081c3b894dea24d1485e0ccdd7e9f93a97b5d9
Atimscreative <timilehinsunday144@gmail.com>
2026-09-21 22:28:56 +0100

    feat: complete Exercise 2.11.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Persons } from "./Persons";
 import { PersonForm } from "./PersonForm";
 import { Filter } from "./Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((data) => setPersons(data.data));
+  }, []);
 
   const filtered = search
     ? persons.filter((person) =>
         person.name.toLowerCase().includes(search.toLowerCase()),
       )
     : persons;
+
+  console.log(persons);
+  console.log(filtered);
 
   const handleOnChange = (event) => {
     setNewName(event.target.value);
